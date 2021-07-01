feaf1ddfec1c7fc9e8eb6512b5ddfed8a922b9e3
markomiz <markomizdrak@gmail.com>
2021-07-01 03:16:44 -0700

    added ball chasing functionality

--- src/ball_chaser/src/drive_bot.cpp
+++ src/ball_chaser/src/drive_bot.cpp
@@ -1,24 +1,24 @@
 #include "ros/ros.h"
 #include "geometry_msgs/Twist.h"
 #include "ball_chaser/DriveToTarget.h"
-//xTODO: Include the ball_chaser "DriveToTarget" header file
 
 // ROS::Publisher motor commands;
 ros::Publisher motor_command_publisher;
 
-// TODO: Create a handle_drive_request callback function that executes whenever a drive_bot service is requested
+// Create a handle_drive_request callback function that executes whenever a drive_bot service is requested
 
 bool handle_drive_request(ball_chaser::DriveToTarget::Request& req,
     ball_chaser::DriveToTarget::Response& res)
 {
+
 	geometry_msgs::Twist motor_command;
 	// Set wheel velocities, forward [0.5, 0.0]
 	motor_command.linear.x = req.linear_x;
 	motor_command.angular.z = req.angular_z;
 	// Publish angles to drive the robot
 	motor_command_publisher.publish(motor_command);
-    ROS_INFO("requested velocities- lin_x:%1.2f, ang_z:%1.2f", (float)req.linear_x, (float)req.angular_z);
-	res.msg_feedback = "velocities set lix_x: " + std::to_string(req.linear_x) + " , ang_z: " + std::to_string(req.angular_z);
+    // ROS_INFO("requested velocities- lin_x:%1.2f, ang_z:%1.2f", (float)req.linear_x, (float)req.angular_z);
+	res.msg_feedback = "velocities set lin_x: " + std::to_string(req.linear_x) + " , ang_z: " + std::to_string(req.angular_z);
     return true;
 }
 
@@ -38,9 +38,9 @@ int main(int argc, char** argv)
     motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 
     // Define a drive /ball_chaser/command_robot service with a handle_drive_request callback function
-    ros::ServiceServer service = n.advertiseService("ball_chaser/comand_robot", handle_drive_request);
+    ros::ServiceServer service = n.advertiseService("ball_chaser/command_robot", handle_drive_request);
 
-    // TODO: Handle ROS communication events
+    //  Handle ROS communication events
     ros::spin();
 
     return 0;
