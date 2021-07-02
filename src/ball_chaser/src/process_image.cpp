dee38e49523735a0904167ccefad09244ba2025e
markomiz <markomizdrak@gmail.com>
2021-07-01 23:22:42 -0700

    adjust white pixel detection to be more precise

--- src/ball_chaser/src/process_image.cpp
+++ src/ball_chaser/src/process_image.cpp
@@ -23,8 +23,8 @@ void process_image_callback(const sensor_msgs::Image img)
     int white_position = -1;
 
      // Loop through each pixel in the image and check if there's a bright white one
-    for (int i = 0; i < img.height * img.step; i++) {
-        if (img.data[i] == white_pixel) {
+    for (int i = 0; i < img.height * img.step; i+=3) {
+        if (img.data[i] == white_pixel && img.data[i+1] == white_pixel && img.data[i+2] == white_pixel) {
             white_position = i;
             break;
         }
